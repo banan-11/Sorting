557e57ce2b66e097a96320353b9b54d2cee8717f
Vlad <vld668@yandex.ru>
2026-04-20 23:01:55 +0300

    что-то там

---
+++ Сортировка пузырьком/Сортировка пузырьком/Program.cs
@@ -0,0 +1,26 @@
+﻿using Сортировка_пузырьком;
+
+namespace Сортировка_пузырьком
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] x = new int[] { 1,6,8,2,8,4,5,3,0,9 };
+
+            var sort1 = new sort_bubbl();
+            sort1.SortBubble(x);
+
+            Console.WriteLine("итог");
+
+            foreach (int n in x)
+            {
+
+                Console.WriteLine(n + " ");
+            }
+
+
+        }
+    }
+}
