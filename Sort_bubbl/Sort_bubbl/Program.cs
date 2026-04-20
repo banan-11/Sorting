9b8b5eb935be10927fe16b58f9d0f96a27717b91
Vlad <vld668@yandex.ru>
2026-04-21 00:52:33 +0300

    Исправление

---
+++ Sort_bubbl/Sort_bubbl/Program.cs
@@ -0,0 +1,23 @@
+﻿using Sort_bubbl;
+
+namespace Sort_bubbl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = new int[] { 1, 6, 8, 2, 8, 4, 5, 3, 0, 9 };
+
+            var sort1 = new Sort_bubbl();
+            sort1.SortBubble(x);
+
+            Console.WriteLine("итог");
+
+            foreach (int n in x)
+            {
+
+                Console.WriteLine(n + " ");
+            }
+        }
+    }
+}
---
+++ Sort_bubbl/Sort_bubbl/Sort_bubbl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sort_bubbl
+{
+    public class Sort_bubbl
+    {
+        public void SortBubble<T>(T[] x) where T : IComparable<T>
+        {
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = 0; j < x.Length - i - 1; j++)
+                {
+                    if (x[j].CompareTo(x[j + 1]) > 0)
+                    {
+
+                        T save = x[j];
+                        x[j] = x[j + 1];
+                        x[j + 1] = save;
+
+
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
