eadc5e34a2b93f115edba432fd96008c10e005b0
Vlad <vld668@yandex.ru>
2026-04-22 00:16:03 +0300

    Добавил комментарии к коду

--- Sort_bubbl/Sort_bubbl/obj/Debug/net10.0/Sort_bubbl.AssemblyInfo.cs
+++ Sort_bubbl/Sort_bubbl/obj/Debug/net10.0/Sort_bubbl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sort_bubbl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b8b5eb935be10927fe16b58f9d0f96a27717b91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sort_bubbl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sort_bubbl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
